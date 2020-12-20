7a86175c0af8e4e17cd7cdcf0c16d0ad3d60189e
jxjilxver <murugin.slava@mail.ru>
2020-12-21 01:24:53 +0300

    16

---
+++ lab16/lab16/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab16
+{
+    public class Tasks
+    {
+        const int capacity = 100;
+        const int density = 1000;
+        const int weight = 6000;
+
+        public static int GetWeight() => capacity * density;
+        public static int GetDensity() => weight / capacity;
+        public static int GetCapacity() => weight / density;
+
+        public static async void Task3()
+        {
+            Task<int> f1 = Task.Factory.StartNew(GetWeight);
+            Task<int> f2 = Task.Factory.StartNew(GetDensity);
+            Task<int> f3 = Task.Factory.StartNew(GetCapacity);
+
+            await f1.ContinueWith((firstTask) => Console.WriteLine($"\nРезультат GetWeight: {firstTask.Result}"));
+            await f2.ContinueWith((secondTask) => Console.WriteLine($"\nРезультат GetDensity: {secondTask.Result}"));
+            await f3.ContinueWith((thirdTask) => Console.WriteLine($"\nРезультат GetCapacity: {thirdTask.Result}"));
+
+            f3.ContinueWith((thirdTask) => Console.WriteLine($"\nРезультат GetCapacity с GetAwaiter(): {thirdTask.Result}")).GetAwaiter();
+            f2.ContinueWith((secondTask) => Console.WriteLine($"\nРезультат GetDensity с GetAwaiter().GetResult(): {secondTask.Result}")).GetAwaiter().GetResult();
+
+        }
+        public static void Task5()
+        {
+            const int arrSize = 10000000;
+            const int arrCount = 10;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            Parallel.For(0, arrCount, (Count) =>
+            {
+                int[] arr = new int[arrSize];
+                Parallel.ForEach(arr, (el) =>
+                {
+                    el = arrCount * arrCount;
+                });
+            });
+            stopwatch.Stop();
+            Console.WriteLine("\nTime with Parallel.For, Parallel.ForEach: " + stopwatch.Elapsed.Milliseconds.ToString());
+            stopwatch.Restart();
+            for (int i = 0; i < arrCount; i++)
+            {
+                int[] arr = new int[arrSize];
+                for (int j = 0; j < arr.Length; j++)
+                    arr[j] = arrCount * arrCount;
+            }
+            stopwatch.Stop();
+            Console.WriteLine("\nTime with two for operators: " + stopwatch.Elapsed.Milliseconds.ToString());
+        }
+        public static void Task6()
+        {
+            Parallel.Invoke(AddInArray, AddInArray, AddInArray);
+        }
+        private static void AddInArray()
+        {
+            int[] arr = new int[1000000];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = i * i;
+            Console.WriteLine("\nParallel.Invoke закончил работу.\n");
+        }
+
+    }
+
+}
---
+++ lab16/lab16/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab16
+{
+    class Program
+    {
+        private const long maxNum = 100000;
+        static BlockingCollection<string> products;
+        static int productNumber = 0;
+        private static CancellationTokenSource source = new CancellationTokenSource();
+        private static CancellationToken token = source.Token;
+        public static void SearchSimpleNumber()
+        {
+            for (long i = 2; i <= maxNum; i++)
+            {
+                if (isSimpleNumber(i))
+                    Console.Write(i + ", ");
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("CancellationRequested\n");
+                    return;
+                }
+            }
+        }
+        private static bool isSimpleNumber(long num)
+        {
+            for (long i = 2; i <= (num / 2); i++)
+                if (num % i == 0)
+                    return false;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int iteration = 3;
+            Stopwatch stopwatch = new Stopwatch();
+            while (iteration > 0)
+            {
+                stopwatch.Start();
+                Task task = Task.Factory.StartNew(SearchSimpleNumber);
+                Console.WriteLine($"\nЗадача {iteration}. ID: {task.Id.ToString()}");
+                Console.WriteLine($"\nЗадача {iteration}. статус: {task.Status.ToString()}");
+                task.Wait();
+                stopwatch.Stop();
+                Console.WriteLine($"\nВремя выполнения задачи {iteration}: {stopwatch.Elapsed.TotalMilliseconds.ToString()}\n");
+                stopwatch.Reset();
+                iteration--;
+                Console.WriteLine("--------------------");
+            }
+
+            /* Stopwatch stopwatch = new Stopwatch();
+             stopwatch.Start();
+             Task task = Task.Factory.StartNew(SearchSimpleNumber);
+             Console.WriteLine($"\nID задачи: {task.Id.ToString()}");
+             Console.WriteLine($"\nСтатус задачи: {task.Status.ToString()}");
+
+             Console.WriteLine("q - для выхода ");
+             if (Console.ReadKey().KeyChar == 'q')
+                 source.Cancel();
+
+             task.Wait();
+             stopwatch.Stop();
+             Console.WriteLine($"\nВремя выполнения задачи: {stopwatch.Elapsed.TotalMilliseconds.ToString()}\n");
+ */
+
+            Tasks.Task3();
+            Tasks.Task5();
+            Tasks.Task6();
+
+
+            products = new BlockingCollection<string>();
+            Task[] cTasks = new Task[5];
+            for (int i = 1; i <= 5; i++)
+            {
+                Task.Factory.StartNew(() =>
+                    new Provider("продавец" + i, i * 1000).Provide(token)
+                );
+                Thread.Sleep(300);
+                cTasks[i - 1] = Task.Factory.StartNew(() =>
+                    new Customer("покупатель" + i, i * 125).Buy()
+                );
+                Thread.Sleep(300);
+            }
+            Thread.Sleep(1000);
+            Task.WaitAll(cTasks);
+            Console.WriteLine("\nПокупатели покинули магазин");
+            source.Cancel();
+        }
+        class Customer
+        {
+            string name;
+            int waitingTime;
+            string product;
+            public void Buy()
+            {
+                while (true)
+                {
+                    if (products.TryTake(out product))
+                    {
+                        Console.WriteLine(name + " купил " + product);
+                    }
+                    else
+                    {
+                        if (products.TryTake(out product, TimeSpan.FromMilliseconds(waitingTime)))
+                        {
+                            Console.WriteLine(name + " купил " + product);
+                        }
+                        else
+                        {
+                            Console.WriteLine(name + " покинул");
+                            break;
+                        }
+                    }
+                    Thread.Sleep(100);
+                }
+            }
+            public Customer(string name, int waitingTime)
+            {
+                this.name = name;
+                this.waitingTime = waitingTime;
+            }
+        }
+        class Provider
+        {
+            string name;
+            int providingTime;
+            string product = "продукт";
+            public void Provide(CancellationToken token)
+            {
+                while (true)
+                {
+                    Thread.Sleep(providingTime);
+                    products.Add($"{product} {++productNumber}");
+                    Console.WriteLine(name + " добавляет " + product + productNumber);
+                    Thread.Sleep(100);
+                    if (token.IsCancellationRequested) break;
+                }
+            }
+            public Provider(string name, int providingTime)
+            {
+                this.name = name;
+                this.providingTime = providingTime;
+            }
+        }
+    }
+}
